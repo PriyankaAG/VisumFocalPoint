12ded24111bd72c7a802847fd4f610637318fec9
amailforsushil <amailforsushil@gmail.com>
2022-05-14 17:12:10 +0530

    Modified dashboard design to latest. Menu option based upon user role.

--- Visum.Services.Mobile.Entities/Security.cs
+++ Visum.Services.Mobile.Entities/Security.cs
@@ -5,6 +5,10 @@
         public enum Areas
         {
             MBL = 325,
+            MBL_Dash = 330,
+            MBL_Dash_CashDrawer = 372,
+            MBL_Dash_RentalVal = 373,
+            MBL_Dash_Revenue = 331,
             MBL_FC = 487,
             MBL_FC_Order = 488,
             MBL_FC_Return = 489,
@@ -21,10 +25,6 @@
             MBL_Inv_Rental_Revenue = 371,
             MBL_Inv_Vendor = 369,
             MBL_Inv_Vendor_Balance = 370,
-            MBL_Dash = 330,
-            MBL_Dash_CashDrawer = 372,
-            MBL_Dash_RentalVal = 373,
-            MBL_Dash_Revenue = 331,
             MBL_TimeClock = 436,
             MBL_TimeClock_BehalfOf = 437
         }
